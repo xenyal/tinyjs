ee6c9ec4ff30b82a9693f6e346efd12796d1fab8
xenyal <vinceliii@hotmail.com>
2016-04-02 18:31:56 -0400

    Added _$ object behaviour

--- tinyjs.js
+++ tinyjs.js
@@ -1,20 +1,44 @@
-if ('querySelector' in document && 'addEventListener' in window) {
-    var _$ = (function() {
-        var elements = document.querySelectorAll(selector);
-        return elements;
-    })
-}
+(function() {    
+    if ('querySelector' in document && 'addEventListener' in window) {
+        var _$ = (function() {
+            var _$ = function() {
+                var elems = document.querySelectorAll(selector);
+                return elems;
+            }
 
-if (typeof _$ !== 'undefined' && typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-    module.exports = _$;
-}
-else {
-   if (typeof define === 'function' && define.amd) {
-      define([], function() {
-        return _$;
-      });
+            _$.prototype.hasClass = function (elem, className) {
+                return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+            };
+
+            _$.prototype.addClass = function (elem, className) {
+                if (!hasClass(elem, className)) {
+                    elem.className += ' ' + className;
+                }
+            };
+
+            _$.prototype.removeClass = function (elem, className) {
+                var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+                if (hasClass(elem, className)) {
+                    while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+                        newClass = newClass.replace(' ' + className + ' ', ' ');
+                    }
+                    elem.className = newClass.replace(/^\s+|\s+$/g, '');
+                }
+            };
+        })();
+    }
+
+    if (typeof _$ !== 'undefined' && typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+        module.exports = _$;
     }
     else {
-      window._$ = _$;
+       if (typeof define === 'function' && define.amd) {
+          define([], function() {
+            return _$;
+          });
+        }
+        else {
+          window._$ = _$;
+        }
     }
-}
+})();
